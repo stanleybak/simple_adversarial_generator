de0db3314258366ea1d25888f856fe6a8f74686e
Stanley Bak <stanleybak@gmail.com>
2021-02-21 13:26:47 -0500

    moved

---
+++ vnncomp_scripts/install_tool.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# example install_tool.sh script for VNNCOMP for simple_adversarial_generator (https://github.com/stanleybak/simple_adversarial_generator) 
+# Stanley Bak, Feb 2021
+
+TOOL_NAME=simple_adv_gen
+VERSION_STRING=v1
+
+# check arguments
+if [ "$1" != ${VERSION_STRING} ]; then
+	echo "Expected first argument (version string) '$VERSION_STRING', got '$1'"
+	exit 1
+fi
+
+echo "Installing $TOOL_NAME"
+cd ..
+pip3 install -r requirements.txt
